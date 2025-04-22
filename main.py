9561d42577cf7a322e5fedfbbcb99d2f0fb2c49d
devam <devammondal06@gmail.com>
2025-04-22 14:19:30 -0400

    cors middleware update

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ from pydantic import BaseModel
 import requests
 import json
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
